08a0934fb87f5f6f0d5752c0d3a9abc3b5447761
Anton Krupin <krupin_anton@mail.ru>
2022-06-22 14:50:57 +0300

    add inputWatcher, feedsWatcher, errorsWatcher

--- src/index.js
+++ src/index.js
@@ -12,25 +12,56 @@ const inputField = document.querySelector('#url-input');
 const sendBtn = document.querySelector('#submit-btn');
 const feedBackField = document.querySelector('.feedback');
 
-const watchedState = onChange(state, (path, value, prev) => {
-  switch (path) {
+const fieldText = [
+  'Ссылка дложна быть валидным URL',
+  'RSS уже существует',
+  'RSS успешно загружен',
+]
+
+const inputWatcher = onChange(state, (path, value, prev) => {
+  console.log('inputwatcher');
+  console.log(path);
+  /*switch (path) {
     case 'urlInput':
+      console.log('urlInput');
       inputField.classList.add('is-invalid');
-      feedBackField.innerText = 'Ссылка дложна быть валидным URL';
+      [feedBackField.innerText] = fieldText;
       break;
     case 'feeds':
+      console.log('feeds');
+      inputField.classList.add('is-valid');
+      [, , feedBackField.innerText] = fieldText;
       break;
     case 'errors':
+      console.log('errors');
       break;
     default:
       throw new Error('Error');
-  }
+  }*/
+});
+
+const feedsWatcher = onChange(state, (path, value, prev) => {
+  inputField.classList.remove('is-invalid');
+  inputField.classList.add('is-valid');
+
+  feedBackField.classList.remove('text-danger');
+  feedBackField.classList.add('text-success');
+
+  [, , feedBackField.innerText] = fieldText;
+});
+
+const errorsWatcher = onChange(state, (path, value, prev) => {
+  inputField.classList.add('is-invalid');
+
+  feedBackField.classList.remove('text-success');
+  feedBackField.classList.add('text-danger');
+
+  [feedBackField.innerText] = fieldText;
+  errorsWatcher.errors = '';
 });
 
 inputField.addEventListener('change', (e) => {
-  if (e.target.value !== '') {
-    watchedState.urlInput = 'filled';
-  }
+  inputWatcher.urlInput = e.target.value;
 });
 
 sendBtn.addEventListener('click', (e) => {
@@ -40,10 +71,10 @@ sendBtn.addEventListener('click', (e) => {
   state.activeLink = inputField.value;
   validate({ link: state.activeLink }).then((el) => {
     if (el.link !== '') {
-      state.feeds.push(el.link);
+      feedsWatcher.feeds.push(el.link);
     }
   }).catch(() => {
-    state.errors = 'Wrong link format';
+    errorsWatcher.errors = 'Wrong link format';
   });
   console.log(state);
 });
