f2fee309e1e5ff768847d72d6c6762269423febe
Anton Krupin <krupin_anton@mail.ru>
2022-06-23 11:31:05 +0300

    add ru.js in src/locales

---
+++ src/locales/ru.js
@@ -0,0 +1,14 @@
+export default {
+  translation: {
+    name: {
+      asc: 'Name (Asc)',
+      desc: 'Name (Desc)',
+      unsorted: 'Name (Unsorted)',
+    },
+    value: {
+      asc: 'Value (Asc)',
+      desc: 'Value (Desc)',
+      unsorted: 'Value (Unsorted)',
+    },
+  },
+};
