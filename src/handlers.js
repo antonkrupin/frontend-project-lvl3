696973033955a7b007f98cc18899be1866e05460
Anton Krupin <krupin_anton@mail.ru>
2022-06-23 20:28:12 +0300

    create handler function

--- src/handlers.js
+++ src/handlers.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import * as yup from 'yup';
 import axios from 'axios';
 
@@ -5,13 +6,13 @@ import parserXML from './parser';
 
 const handler = (event, state) => {
   event.preventDefault();
-  // eslint-disable-next-line no-param-reassign
+
   state.formStatus = 'processing';
+
   const formData = new FormData(event.target);
   const link = formData.get('url').trim();
 
   const rssValidateSchema = yup.object().shape({
-    // разобраться с работой notOneOf
     link: yup.string().url().notOneOf(state.feeds),
   });
 
@@ -27,28 +28,22 @@ const handler = (event, state) => {
     // test link, don`t forget remove after end project;
     const testLink = 'http://lorem-rss.herokuapp.com/feed';
     const rssLink = `https://allorigins.hexlet.app/get?disableCache=true&url=${testLink}`;
-
+    state.errorValue = 'Rss добавлен';
     axios({
       method: 'get',
       url: rssLink,
     }).then((response) => {
       const data = response.data.contents;
       parserXML(data);
-      // eslint-disable-next-line no-param-reassign
       state.feeds.push(link);
-      // eslint-disable-next-line no-param-reassign
       state.formStatus = 'processed';
     }).catch((axiosError) => {
-      // eslint-disable-next-line no-param-reassign
       state.errorValue = axiosError;
-      // eslint-disable-next-line no-param-reassign
       state.formStatus = 'failure';
     });
   }).catch((error) => {
-    // eslint-disable-next-line no-param-reassign
+    state.errorValue = error.errors;
     state.formStatus = 'failure';
-    console.log(error.name);
-    console.log(error.errors);
   });
 };
 
