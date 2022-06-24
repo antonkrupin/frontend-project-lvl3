e2ae2fe71e88fd9c2b3b8c41f7455f7aeb3c7482
Anton Krupin <krupin_anton@mail.ru>
2022-06-24 16:22:18 +0300

    create function createMarkupFeed

--- src/renders.js
+++ src/renders.js
@@ -1,36 +1,59 @@
+const createMarkupFeed = (text) => {
+  /*const div = document.createElement('div');
+  div.classList.add('row', 'feed');*/
+
+  const row = document.createElement('div');
+  row.classList.add('row');
+
+  const col = document.createElement('div');
+  col.classList.add('col');
+
+  const h3 = document.createElement('h3');
+  h3.classList.add('feedTitle');
+  h3.textContent = text;
+
+  col.append(h3);
+  row.append(col);
+  
+  return row;
+};
+
+const renderPost = () => {
+
+};
+
+const renderFeed = (title, description) => {
+  const feedsSection = document.querySelector('.feed');
+  feedsSection.prepend(createMarkupFeed(description));
+  feedsSection.prepend(createMarkupFeed(title));
+  /*feedsSection.querySelector('.feedTitle').textContent = title;
+  feedsSection.querySelector('.feedDescription').textContent = description;*/
+  //createMarkup();
+};
+
 const renderFeeds = (state) => {
   state.map((el) => {
     if (!el.render) {
-      console.log(Object.keys(el));
+      renderFeed(el.title, el.description);
+      el.render = true;
     }
   });
 };
 
-const renderPost = () => {
-  console.log('test');
-};
-
-const renderFeed = () => {
-  console.log('test');
-};
-
-export default {
-  renderFeeds,
-  renderPost,
-  renderFeed,
-};
+export default renderFeeds;
 
 /*
-<div class="col-8" id="posts">
-<h1>Посты</h1>
-<div class="row post">
-    <div class="col-9">
-        <h4>Тема поста</h4>
+<div class="row feed">
+    <div class="row">
+        <div class="col-9">
+            <h3 class="feedTitle">Title</h3>
+        </div>
     </div>
-    <div class="col-3">
-        <button class="btn btn-outline-primary">Просмотр</button>
+    <div class="row">
+        <div class="col-9">
+            <h4 class="feedDescription">Description</h4>
+        </div>
     </div>
-</div>
 </div>
 
 */
