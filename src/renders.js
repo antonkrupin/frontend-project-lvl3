2aafb848c6b69eca8e8bc664788277e751018b57
Anton Krupin <krupin_anton@mail.ru>
2022-06-24 12:00:10 +0300

    add renderPost, renderFeed functions

--- src/renders.js
+++ src/renders.js
@@ -1,7 +1,36 @@
 const renderFeeds = (state) => {
   state.map((el) => {
-    console.log(el);
+    if (!el.render) {
+      console.log(Object.keys(el));
+    }
   });
 };
 
-export default renderFeeds;
+const renderPost = () => {
+  console.log('test');
+};
+
+const renderFeed = () => {
+  console.log('test');
+};
+
+export default {
+  renderFeeds,
+  renderPost,
+  renderFeed,
+};
+
+/*
+<div class="col-8" id="posts">
+<h1>Посты</h1>
+<div class="row post">
+    <div class="col-9">
+        <h4>Тема поста</h4>
+    </div>
+    <div class="col-3">
+        <button class="btn btn-outline-primary">Просмотр</button>
+    </div>
+</div>
+</div>
+
+*/
