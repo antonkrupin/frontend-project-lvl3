0e0d6775b6ab0f16b161a42a91c98b5bcd90bfd2
Anton Krupin <krupin_anton@mail.ru>
2022-06-24 08:59:55 +0300

    add nerworkProblems key in Errors

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -3,6 +3,7 @@ export default {
     errors: {
       urlFormat: 'Ссылка должна быть валидным URL',
       urlRepeat: 'RSS уже существует',
+      networkProblems: 'Проблемы с загрузкой RSS',
     },
     urlAdded: 'RSS успешно загружен',
   },
