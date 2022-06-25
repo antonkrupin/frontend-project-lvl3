e1a5cf56fcb4f5f3b3bea27ddc4dbeba807be640
Anton Krupin <krupin_anton@mail.ru>
2022-06-25 13:32:11 +0300

    change functions createMarkupFeed, createMarkupPost

--- src/renders.js
+++ src/renders.js
@@ -1,57 +1,93 @@
 /* eslint-disable no-param-reassign */
-const createMarkupFeed = (text) => {
+const createMarkupFeed = (title, description) => {
   const row = document.createElement('div');
-  row.classList.add('row');
+  row.classList.add('row', 'feed');
 
-  const col = document.createElement('div');
-  col.classList.add('col');
+  const titleRow = document.createElement('div');
+  titleRow.classList.add('row');
 
-  const h3 = document.createElement('h3');
-  h3.classList.add('feedTitle');
-  h3.textContent = text;
+  const titleCol = document.createElement('div');
+  titleCol.classList.add('col');
 
-  col.append(h3);
-  row.append(col);
+  const descriptionRow = document.createElement('div');
+  descriptionRow.classList.add('row');
+
+  const descriptionCol = document.createElement('div');
+  descriptionCol.classList.add('col');
+
+  const h3Title = document.createElement('h3');
+  h3Title.textContent = title;
+
+  const h3Deiscription = document.createElement('h3');
+  h3Deiscription.textContent = description;
+
+  titleCol.append(h3Title);
+  descriptionCol.append(h3Deiscription);
+  titleRow.append(titleCol);
+  descriptionRow.append(descriptionCol);
+
+  row.append(titleRow);
+  row.append(descriptionRow);
 
   return row;
 };
 
-const renderPost = () => {
+// перекинуть функции
+// создания разметки в отдельную папку
 
+const createMarkupPost = (text, link, buttonText = 'Просмотр') => {
+  const row = document.createElement('div');
+  row.classList.add('row', 'post');
+
+  const themeCol = document.createElement('div');
+  themeCol.classList.add('col-9');
+
+  const themeH4 = document.createElement('h4');
+
+  const themeLink = document.createElement('a');
+  themeLink.setAttribute('href', link);
+  themeLink.setAttribute('target', '_blank');
+  themeLink.textContent = text;
+
+  const buttonCol = document.createElement('div');
+  buttonCol.classList.add('col-3');
+
+  const button = document.createElement('button');
+  button.classList.add('btn', 'btn-outline-primary');
+  button.textContent = buttonText;
+
+  themeH4.append(themeLink);
+  themeCol.append(themeH4);
+  buttonCol.append(button);
+  row.append(themeCol);
+  row.append(buttonCol);
+
+  return row;
+};
+
+// добавить разделение по фидам
+// чтобы все посты от одного фида были в одной секции
+
+const renderPost = (theme, link) => {
+  const postsSection = document.querySelector('#posts');
+  postsSection.append(createMarkupPost(theme, link));
 };
 
 const renderFeed = (title, description) => {
-  const feedsSection = document.querySelector('.feed');
-  feedsSection.prepend(createMarkupFeed(description));
-  feedsSection.prepend(createMarkupFeed(title));
-  /*feedsSection.querySelector('.feedTitle').textContent = title;
-  feedsSection.querySelector('.feedDescription').textContent = description;*/
-  //createMarkup();
+  const feedsSection = document.querySelector('#feeds');
+  feedsSection.prepend(createMarkupFeed(title, description));
 };
 
 const renderFeeds = (state) => {
   state.map((el) => {
     if (!el.render) {
       renderFeed(el.title, el.description);
+      el.items.map((item) => {
+        renderPost(item[0], item[2]);
+      });
       el.render = true;
     }
   });
 };
 
 export default renderFeeds;
-
-/*
-<div class="row feed">
-    <div class="row">
-        <div class="col-9">
-            <h3 class="feedTitle">Title</h3>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-9">
-            <h4 class="feedDescription">Description</h4>
-        </div>
-    </div>
-</div>
-
-*/
