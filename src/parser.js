7c5d3cce46528338466f0d9b26fd55afa12f6b48
Anton Krupin <krupin_anton@mail.ru>
2022-09-06 19:28:53 +0300

    remove comments

--- src/parser.js
+++ src/parser.js
@@ -10,18 +10,6 @@ const parserXML = (data, rssLink) => {
     link, rssLink, title, description,
   };
 
-  /* const posts = {};
-  posts[id] = [];
-
-  channel.querySelectorAll('item').forEach((elem) => {
-    const postTitle = elem.querySelector('title').textContent;
-    const postDescription = elem.querySelector('description').textContent;
-    const postLink = elem.querySelector('link').textContent;
-    const postDate = elem.querySelector('pubDate').textContent;
-    posts[id].push({
-      rssLink, postTitle, postDescription, postLink, postDate,
-    });
-  }); */
   const posts = [];
   channel.querySelectorAll('item').forEach((elem) => {
     const postTitle = elem.querySelector('title').textContent;
