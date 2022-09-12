36c47d93d2497c5d6aed68ca1e5fc1d92456eaf9
antonkrupin <krupin_anton@mail.ru>
2022-09-12 11:20:37 +0300

    change

--- src/renders.js
+++ src/renders.js
@@ -38,14 +38,22 @@ const buttonHandler = (button) => {
 
 const renderMarkupFeed = (title, description) => {
   const li = document.createElement('li');
-  li.classList.add('list-group-item', 'border-0', 'border-end-0');
+  li.classList.add(
+    'list-group-item',
+    'border-0',
+    'border-end-0',
+  );
 
   const h3 = document.createElement('h3');
   h3.classList.add('h6', 'm-0');
   h3.textContent = title;
 
   const p = document.createElement('p');
-  p.classList.add('m-0', 'small', 'text-black-50');
+  p.classList.add(
+    'm-0',
+    'small',
+    'text-black-50',
+  );
   p.textContent = description;
 
   li.append(h3);
